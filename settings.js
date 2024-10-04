45461d622fa248229ffe06c78b7d11803c54bd79
abuhurairah786 <103510868+abuhurairah786@users.noreply.github.com>
2024-10-04 12:28:01 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -4,24 +4,24 @@ const fs = require('fs');
 const chalk = require('chalk');
 
 //owmner v card
-global.ytname = "YT: GlobalTechInfo" //ur yt chanel name
-global.socialm = "GitHub: GlobalTechInfo" //ur github or insta name
-global.location = "Pakistan, Karachi, Karachi" //ur location
+global.ytname = "YT: @HURAIRAH" //ur yt chanel name
+global.socialm = "GitHub: @HURAIRAH" //ur github or insta name
+global.location = "Pakistan" //ur location
 
 //new
-global.botname = '© 𝐆𝐋𝐎𝐁𝐀𝐋-𝐌𝐃' //ur bot name
-global.ownernumber = ['923444844060'] //ur owner number, dont add more than one
-global.ownername = '© GlobalTechInfo' //ur owner name
-global.websitex = "https://www.youtube.com/@GlobalTechInfo"
-global.wagc = "https://whatsapp.com/channel/0029VagJIAr3bbVBCpEkAM07"
+global.botname = '🅗🅤🅡🅐🅘🅡🅐🅗-🅜🅓' //ur bot name
+global.ownernumber = ['923449412639'] //ur owner number, dont add more than one
+global.ownername = 'ABUHURAIRAH' //ur owner name
+global.websitex = "https://www.youtube.com/@HURAIRAH"
+global.wagc = "https://whatsapp.com/channel/@HURAIRAH"
 global.themeemoji = '🪀'
-global.wm = "Global Bot Inc."
-global.botscript = 'https://github.com/GlobalTechInfo/GLOBAL-MD' //script link
+global.wm = "GlobaL HURAIRAH MD"
+global.botscript = 'https://github.com/HURAIRAH' //script link
 global.packname = "Sticker By"
-global.author = "GlobalTechInfo\n\n+923444844060"
-global.creator = "923444844060@s.whatsapp.net"
+global.author = "ABUHURAIRAH\n\n+923449412639"
+global.creator = "923449412639@s.whatsapp.net"
 global.xprefix = '.'
-global.premium = ["923444844060"] // Premium User
+global.premium = ["923449412639"] // Premium User
 
 //channel id
 global.xchannel = {
@@ -32,7 +32,7 @@ global.xchannel = {
 global.typemenu = 'v12' // menu type 'v1' => 'v12'
 global.typereply = 'v4' // reply type 'v1' => 'v4'
 global.autoblocknumber = '91' //set autoblock country code
-global.antiforeignnumber = '91' //set anti foreign number country code
+global.antiforeignnumber = '0' //set anti foreign number country code
 
 global.listv = ['➜','☞','㉨','✪','✯','益','⊡','☆','ෆ','✎','⎆','⫹⫺','■','✿','▲','➩','➢','➣','➤','✦','✧','△','❀','○','□','♤','♡','◇','♧','々','〆','֎','ᚏ','⸙','⧉']
 global.tempatDB = 'database.json'
